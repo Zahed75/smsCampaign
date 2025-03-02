c8cebcb1c0bf1b837b0363b59483ecfef0742cdc
Zahed75 <zahedhasan.cs@gmail.com>
2025-03-03 02:15:15 +0600

    first commit

---
+++ customerGift/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from customerGift.views import *
+
+
+
+urlpatterns = [
+
+
+]
+
+
---
+++ smsCampaign/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('admin/', admin.site.urls),
+
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
